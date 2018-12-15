12d4201887cb25a828b64cc8c812bbe7586a382a
sigmanigma <bourqueno77@yahoo.com>
2018-12-14 23:09:06 -0800

    More addition to Equipment Return
    Began to add MySQL.  Format more details.  Will need help finalizing MySQL part to make the method it adds back to the database correct.

--- GB Manufacturing IMS/EquipReturn.Designer.cs
+++ GB Manufacturing IMS/EquipReturn.Designer.cs
@@ -30,21 +30,23 @@
         {
             this.lblReturnTitle = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lblReturnStatus = new System.Windows.Forms.Label();
+            this.btnReturnSubmit = new System.Windows.Forms.Button();
+            this.txtReturnItem = new System.Windows.Forms.TextBox();
+            this.comboStatus = new System.Windows.Forms.ComboBox();
             this.lblReturnItem = new System.Windows.Forms.Label();
+            this.lblReturnStatus = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboStatus = new System.Windows.Forms.ComboBox();
-            this.txtReturnItem = new System.Windows.Forms.TextBox();
-            this.btnReturnSubmit = new System.Windows.Forms.Button();
             this.lblRetItemChk = new System.Windows.Forms.Label();
             this.lblReturnStatChk = new System.Windows.Forms.Label();
             this.lblReturnJob = new System.Windows.Forms.Label();
             this.lblChkBox1 = new System.Windows.Forms.Label();
             this.lblChkBox2 = new System.Windows.Forms.Label();
-            this.lblChkBox3 = new System.Windows.Forms.Label();
             this.btnReturnConfirm = new System.Windows.Forms.Button();
+            this.lblReturnError = new System.Windows.Forms.Label();
+            this.dgReturnJob = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgReturnJob)).BeginInit();
             this.SuspendLayout();
             // 
             // lblReturnTitle
@@ -59,6 +61,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.lblReturnError);
             this.groupBox1.Controls.Add(this.btnReturnSubmit);
             this.groupBox1.Controls.Add(this.txtReturnItem);
             this.groupBox1.Controls.Add(this.comboStatus);
@@ -72,14 +75,36 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Equipment Return Details";
             // 
-            // lblReturnStatus
+            // btnReturnSubmit
             // 
-            this.lblReturnStatus.AutoSize = true;
-            this.lblReturnStatus.Location = new System.Drawing.Point(19, 37);
-            this.lblReturnStatus.Name = "lblReturnStatus";
-            this.lblReturnStatus.Size = new System.Drawing.Size(90, 16);
-            this.lblReturnStatus.TabIndex = 0;
-            this.lblReturnStatus.Text = "Status of Item:";
+            this.btnReturnSubmit.Location = new System.Drawing.Point(187, 199);
+            this.btnReturnSubmit.Name = "btnReturnSubmit";
+            this.btnReturnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnReturnSubmit.TabIndex = 4;
+            this.btnReturnSubmit.Text = "Check In";
+            this.btnReturnSubmit.UseVisualStyleBackColor = true;
+            this.btnReturnSubmit.Click += new System.EventHandler(this.btnReturnSubmit_Click);
+            // 
+            // txtReturnItem
+            // 
+            this.txtReturnItem.Location = new System.Drawing.Point(119, 80);
+            this.txtReturnItem.Name = "txtReturnItem";
+            this.txtReturnItem.Size = new System.Drawing.Size(143, 22);
+            this.txtReturnItem.TabIndex = 3;
+            // 
+            // comboStatus
+            // 
+            this.comboStatus.FormattingEnabled = true;
+            this.comboStatus.Items.AddRange(new object[] {
+            "Working",
+            "Not Working",
+            "Missing",
+            "Stolen"});
+            this.comboStatus.Location = new System.Drawing.Point(120, 34);
+            this.comboStatus.Name = "comboStatus";
+            this.comboStatus.Size = new System.Drawing.Size(142, 24);
+            this.comboStatus.TabIndex = 2;
+            this.comboStatus.SelectedIndexChanged += new System.EventHandler(this.comboStatus_SelectedIndexChanged);
             // 
             // lblReturnItem
             // 
@@ -90,15 +115,24 @@
             this.lblReturnItem.TabIndex = 1;
             this.lblReturnItem.Text = "Item Number:";
             // 
+            // lblReturnStatus
+            // 
+            this.lblReturnStatus.AutoSize = true;
+            this.lblReturnStatus.Location = new System.Drawing.Point(19, 37);
+            this.lblReturnStatus.Name = "lblReturnStatus";
+            this.lblReturnStatus.Size = new System.Drawing.Size(90, 16);
+            this.lblReturnStatus.TabIndex = 0;
+            this.lblReturnStatus.Text = "Status of Item:";
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(29, 334);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(413, 20);
+            this.label3.Size = new System.Drawing.Size(431, 20);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Please make sure all equipment is return in working order.";
+            this.label3.Text = "Please make sure all equipment is returned in working order.";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
@@ -111,41 +145,11 @@
             this.label4.TabIndex = 3;
             this.label4.Text = "Equipment needs to be cleaned and ready to use when checked in.";
             // 
-            // comboStatus
-            // 
-            this.comboStatus.FormattingEnabled = true;
-            this.comboStatus.Items.AddRange(new object[] {
-            "In Stock",
-            "Shrink",
-            "Lost"});
-            this.comboStatus.Location = new System.Drawing.Point(120, 34);
-            this.comboStatus.Name = "comboStatus";
-            this.comboStatus.Size = new System.Drawing.Size(142, 24);
-            this.comboStatus.TabIndex = 2;
-            this.comboStatus.SelectedIndexChanged += new System.EventHandler(this.comboStatus_SelectedIndexChanged);
-            // 
-            // txtReturnItem
-            // 
-            this.txtReturnItem.Location = new System.Drawing.Point(119, 80);
-            this.txtReturnItem.Name = "txtReturnItem";
-            this.txtReturnItem.Size = new System.Drawing.Size(143, 22);
-            this.txtReturnItem.TabIndex = 3;
-            // 
-            // btnReturnSubmit
-            // 
-            this.btnReturnSubmit.Location = new System.Drawing.Point(187, 199);
-            this.btnReturnSubmit.Name = "btnReturnSubmit";
-            this.btnReturnSubmit.Size = new System.Drawing.Size(75, 23);
-            this.btnReturnSubmit.TabIndex = 4;
-            this.btnReturnSubmit.Text = "Check In";
-            this.btnReturnSubmit.UseVisualStyleBackColor = true;
-            this.btnReturnSubmit.Click += new System.EventHandler(this.btnReturnSubmit_Click);
-            // 
             // lblRetItemChk
             // 
             this.lblRetItemChk.AutoSize = true;
             this.lblRetItemChk.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRetItemChk.Location = new System.Drawing.Point(329, 160);
+            this.lblRetItemChk.Location = new System.Drawing.Point(343, 157);
             this.lblRetItemChk.Name = "lblRetItemChk";
             this.lblRetItemChk.Size = new System.Drawing.Size(87, 16);
             this.lblRetItemChk.TabIndex = 6;
@@ -157,15 +161,15 @@
             this.lblReturnStatChk.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblReturnStatChk.Location = new System.Drawing.Point(324, 114);
             this.lblReturnStatChk.Name = "lblReturnStatChk";
-            this.lblReturnStatChk.Size = new System.Drawing.Size(90, 16);
+            this.lblReturnStatChk.Size = new System.Drawing.Size(106, 16);
             this.lblReturnStatChk.TabIndex = 5;
-            this.lblReturnStatChk.Text = "Status of Item:";
+            this.lblReturnStatChk.Text = "Return as status:";
             // 
             // lblReturnJob
             // 
             this.lblReturnJob.AutoSize = true;
             this.lblReturnJob.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblReturnJob.Location = new System.Drawing.Point(344, 204);
+            this.lblReturnJob.Location = new System.Drawing.Point(360, 222);
             this.lblReturnJob.Name = "lblReturnJob";
             this.lblReturnJob.Size = new System.Drawing.Size(70, 16);
             this.lblReturnJob.TabIndex = 7;
@@ -185,26 +189,16 @@
             // 
             this.lblChkBox2.AutoSize = true;
             this.lblChkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblChkBox2.Location = new System.Drawing.Point(441, 160);
+            this.lblChkBox2.Location = new System.Drawing.Point(441, 157);
             this.lblChkBox2.Name = "lblChkBox2";
             this.lblChkBox2.Size = new System.Drawing.Size(17, 16);
             this.lblChkBox2.TabIndex = 9;
             this.lblChkBox2.Text = "...";
             // 
-            // lblChkBox3
-            // 
-            this.lblChkBox3.AutoSize = true;
-            this.lblChkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblChkBox3.Location = new System.Drawing.Point(441, 204);
-            this.lblChkBox3.Name = "lblChkBox3";
-            this.lblChkBox3.Size = new System.Drawing.Size(17, 16);
-            this.lblChkBox3.TabIndex = 10;
-            this.lblChkBox3.Text = "...";
-            // 
             // btnReturnConfirm
             // 
             this.btnReturnConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReturnConfirm.Location = new System.Drawing.Point(490, 276);
+            this.btnReturnConfirm.Location = new System.Drawing.Point(487, 276);
             this.btnReturnConfirm.Name = "btnReturnConfirm";
             this.btnReturnConfirm.Size = new System.Drawing.Size(75, 23);
             this.btnReturnConfirm.TabIndex = 5;
@@ -212,13 +206,39 @@
             this.btnReturnConfirm.UseVisualStyleBackColor = true;
             this.btnReturnConfirm.Click += new System.EventHandler(this.btnReturnConfirm_Click);
             // 
+            // lblReturnError
+            // 
+            this.lblReturnError.AutoSize = true;
+            this.lblReturnError.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReturnError.ForeColor = System.Drawing.Color.Red;
+            this.lblReturnError.Location = new System.Drawing.Point(24, 145);
+            this.lblReturnError.Name = "lblReturnError";
+            this.lblReturnError.Size = new System.Drawing.Size(0, 18);
+            this.lblReturnError.TabIndex = 5;
+            // 
+            // dgReturnJob
+            // 
+            this.dgReturnJob.AllowUserToAddRows = false;
+            this.dgReturnJob.AllowUserToDeleteRows = false;
+            this.dgReturnJob.AllowUserToResizeColumns = false;
+            this.dgReturnJob.AllowUserToResizeRows = false;
+            this.dgReturnJob.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgReturnJob.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgReturnJob.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgReturnJob.ColumnHeadersVisible = false;
+            this.dgReturnJob.Location = new System.Drawing.Point(444, 198);
+            this.dgReturnJob.Name = "dgReturnJob";
+            this.dgReturnJob.RowHeadersVisible = false;
+            this.dgReturnJob.Size = new System.Drawing.Size(118, 58);
+            this.dgReturnJob.TabIndex = 10;
+            // 
             // EquipReturn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(604, 418);
+            this.Controls.Add(this.dgReturnJob);
             this.Controls.Add(this.btnReturnConfirm);
-            this.Controls.Add(this.lblChkBox3);
             this.Controls.Add(this.lblChkBox2);
             this.Controls.Add(this.lblChkBox1);
             this.Controls.Add(this.lblReturnJob);
@@ -236,6 +256,7 @@
             this.Load += new System.EventHandler(this.EquipReturn_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgReturnJob)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -257,7 +278,8 @@
         private System.Windows.Forms.Label lblReturnJob;
         private System.Windows.Forms.Label lblChkBox1;
         private System.Windows.Forms.Label lblChkBox2;
-        private System.Windows.Forms.Label lblChkBox3;
         private System.Windows.Forms.Button btnReturnConfirm;
+        private System.Windows.Forms.Label lblReturnError;
+        private System.Windows.Forms.DataGridView dgReturnJob;
     }
 }
--- GB Manufacturing IMS/EquipReturn.cs
+++ GB Manufacturing IMS/EquipReturn.cs
@@ -33,12 +33,56 @@ namespace GB_Manufacturing_IMS
         private void btnReturnSubmit_Click(object sender, EventArgs e)
         {
             //Passes values to right and adds Job Code so User can confirm he is returning the correct equipment
-            lblChkBox1.Text = comboStatus.Text;
-            comboStatus.Text = "";
-            lblChkBox2.Text = txtReturnItem.Text;
-            txtReturnItem.Text = "";
+            bool comboEmpty = true;
+        
+            if (comboStatus.Text != "")
+            {
+                comboEmpty = false;
+            }
             //Job Code query to add to right side of view to confirm return.
-            
+            if (comboEmpty == false && txtReturnItem.Text != "")
+            {
+                switch (comboStatus.Text)
+                {
+                    case "Working":
+                        {
+                            lblChkBox1.Text = "In Stock";
+                            break;
+                        }
+                    case "Not Working":
+                        {
+                            lblChkBox1.Text = "Damaged";
+                            break;
+                        }
+                    case "Missing":
+                        {
+                            lblChkBox1.Text = "Lost";
+                            break;
+                        }
+                    case "Stolen":
+                        {
+                            lblChkBox1.Text = "Shrink";
+                            break;
+                        }
+                }
+                comboStatus.Text = "";
+                lblChkBox2.Text = txtReturnItem.Text;
+                txtReturnItem.Text = "";
+
+                string input = lblChkBox2.Text;
+                string query = "SELECT jobCode FROM EquipmentRental WHERE equipmentID = " + input + "";
+                mydb.fill(dgReturnJob, query);
+                lblReturnError.Text = "";
+            }
+            else
+            {
+                lblReturnError.Text = "Please fill out both fields.";
+                txtReturnItem.Text = "";
+                comboStatus.Text = "";
+                lblChkBox1.Text = "";
+                lblChkBox2.Text = "";
+                //dgReturnJob = null;
+            }
         }
 
         private void comboStatus_SelectedIndexChanged(object sender, EventArgs e)
@@ -53,7 +97,6 @@ namespace GB_Manufacturing_IMS
             //Empties field so it is ready for another return
             lblChkBox1.Text = "";
             lblChkBox2.Text = "";
-            lblChkBox3.Text = "";
             
         }
     }
